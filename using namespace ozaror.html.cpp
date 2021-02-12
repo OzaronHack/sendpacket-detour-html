ee69828e65f17177d275b92f88d0c8fc48c2d2e1
OzaronHack <78981138+OzaronHack@users.noreply.github.com>
2021-02-12 23:37:20 +0400

    Create using namespace ozaror.html.cpp

---
+++ using namespace ozaror.html.cpp
@@ -0,0 +1,16 @@
+// DISCORD (Ozaron#5101) MADE BY OZARON (OZARON HACK) DON"T DM ME IM BUSY OF DOING CHIT.
+
+using namespace OZARON_HACK;
+
+#define OZA/*OZARON#5101 MADE IT*/RON5101(51101 OZARON)
+
+OZARON Main() {
+  __stdcall OZARON(){}]->esp(*b/*Github in descripshn sobscribe*/SendPacket&&&&)
+  _SendPacket** BSend/*TODO: REWRITE VARS NAMES*/bSednPAcke()->esp.send( bSendPacket() )
+
+  /* HACKED BAYONET BY OZAROM#5501 */
+  
+  OZARON->BAYONET.HACK();
+
+  return TRUE(OZARON);
+}
